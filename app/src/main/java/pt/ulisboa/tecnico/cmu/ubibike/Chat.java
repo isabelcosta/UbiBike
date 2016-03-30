7e6ceefc176e4e0909d88d1ca9a6fbb0cc1bee64
vicenterocha <vicente_c_rocha@hotmail.com>
2016-03-30 13:25:58 +0100

    Chat melhorado

--- app/src/main/java/pt/ulisboa/tecnico/cmu/ubibike/Chat.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmu/ubibike/Chat.java
@@ -1,8 +1,11 @@
 package pt.ulisboa.tecnico.cmu.ubibike;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,6 +20,7 @@ public class Chat extends AppCompatActivity {
     private EditText editTxt;
     private Button btn;
     private ListView list;
+    private EditText textMsg;
     private ArrayAdapter<String> adapter;
     private ArrayList<String> arrayList;
     private String bikerName;
@@ -35,8 +39,10 @@ public class Chat extends AppCompatActivity {
 
 
 
-        btn = (Button) findViewById(R.id.button);
-        list = (ListView) findViewById(R.id.listView);
+        btn = (Button) findViewById(R.id.btSend1);
+        list = (ListView) findViewById(R.id.lvChat);
+        textMsg = (EditText) findViewById(R.id.etMessage1);
+
         arrayList = new ArrayList<String>();
 
         // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
@@ -44,15 +50,38 @@ public class Chat extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
 
         // Here, you set the data in your ListView
-        list.setAdapter(adapter);
+//        list.setAdapter(adapter);
+
+        final ChatListAdapter mAdapter;
+        final ArrayList<Message> mMessages;
+        mMessages = new ArrayList<>();
+
+        mAdapter = new ChatListAdapter(Chat.this, "Vicente", mMessages);
+        list.setAdapter(mAdapter);
+
+        list.setBackgroundColor(getResources().getColor(R.color.grey));
+
 
         btn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                Message m = new Message();
+                m.setBody(textMsg.getText().toString());
+                m.setUserId("Antonio");
+
                 // this line adds the data of your EditText and puts in your array
-                arrayList.add("test");
+                mMessages.add(m);
                 // next thing you have to do is check if your adapter has changed
-                adapter.notifyDataSetChanged();
+                mAdapter.notifyDataSetChanged();
+
+                m = new Message();
+                m.setBody("Auto text Back");
+                m.setUserId("Joao");
+                // this line adds the data of your EditText and puts in your array
+                mMessages.add(m);
+
+                textMsg.setText(null);
             }
         });
 
@@ -62,8 +91,10 @@ public class Chat extends AppCompatActivity {
         messengerBtn.setTextColor(getResources().getColor(R.color.white));
 
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 
     public void launchClick(View v) {
         Intent intent = null;
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmu/ubibike/ChatListAdapter.java
@@ -0,0 +1,69 @@
+package pt.ulisboa.tecnico.cmu.ubibike;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.List;
+
+/**
+ * Created by vicente on 30-Mar-16.
+ */
+public class ChatListAdapter extends ArrayAdapter<Message> {
+    private String mUserId;
+
+    public ChatListAdapter(Context context, String userId, List<Message> messages) {
+        super(context, 0, messages);
+        this.mUserId = userId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).
+                    inflate(R.layout.chat_item, parent, false);
+            final ViewHolder holder = new ViewHolder();
+            holder.imageOther = (ImageView)convertView.findViewById(R.id.ivProfileOther);
+            holder.imageMe = (ImageView)convertView.findViewById(R.id.ivProfileMe);
+            holder.body = (TextView)convertView.findViewById(R.id.tvBody);
+            convertView.setTag(holder);
+        }
+        final Message message = getItem(position);
+        final ViewHolder holder = (ViewHolder)convertView.getTag();
+//        final boolean isMe = message.getUserId().equals(mUserId);
+        Boolean isMe = true;
+        if (position % 2 == 0) {
+            // even
+            isMe = false;
+        }
+        // Show-hide image based on the logged-in user.
+        // Display the profile image to the right for our user, left for other users.
+        if (isMe) {
+            holder.imageMe.setVisibility(View.VISIBLE);
+            holder.imageOther.setVisibility(View.GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.END);
+        } else {
+            holder.imageOther.setVisibility(View.VISIBLE);
+            holder.imageMe.setVisibility(View.GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.START);
+        }
+        final ImageView profileView = isMe ? holder.imageMe : holder.imageOther;
+
+        holder.body.setText(message.getBody());
+        return convertView;
+    }
+
+
+    final class ViewHolder {
+        public ImageView imageOther;
+        public ImageView imageMe;
+        public TextView body;
+    }
+}
