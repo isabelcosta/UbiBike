73368d38666fc075a6bdcdfde643398c6988a00b
Isabel Costa <isabelcmdcosta@gmail.com>
2016-04-01 02:57:37 +0100

    organize code on application class

--- app/src/main/java/pt/ulisboa/tecnico/cmu/ubibike/UbiBikeApplication.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmu/ubibike/UbiBikeApplication.java
@@ -2,25 +2,35 @@ package pt.ulisboa.tecnico.cmu.ubibike;
 
 import android.app.Application;
 
-/**
- * Created by Isabel on 01/04/2016.
- */
+
 public class UbiBikeApplication extends Application {
 
     private String _username;
     private int _score;
     private boolean _status;
 
+    //Getters
+
     public String get_username() {
         return _username;
     }
+
+    public boolean is_status() {
+        return _status;
+    }
+
+    public int get_score() {
+        return _score;
+    }
+
+    //Setters
 
     public void set_username(String username) {
         this._username = username;
     }
 
-    public int get_score() {
-        return _score;
+    public void set_status(boolean status) {
+        this._status = status;
     }
 
     public void set_score(int score) {
